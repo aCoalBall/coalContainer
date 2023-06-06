5edaad310cbc4fa61bc88675c6109ba65d893b25
root <root@coalball.localdomain>
2023-06-06 18:51:17 +0900

    update

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"os/exec"
 	"syscall"
@@ -10,6 +9,7 @@ import (
 
 // go run coal main.go
 func main() {
+	fmt.Println(os.Args[1])
 	switch os.Args[1] {
 	case "run":
 		run()
@@ -21,8 +21,7 @@ func main() {
 }
 
 func run() {
-	fmt.Printf("Running %v as process %d\n", os.Args[2:], os.Getpid())
-	cmd := exec.Command("/proc/self/exe", append([]string{}, os.Args[2:]...)...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
@@ -30,13 +29,25 @@ func run() {
 		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
 	}
 
-	syscall.Chroot()
-	if err := cmd.Run(); err != nil {
-		log.Fatal("Error: ", err)
-	}
-
+	//syscall.Chroot()
+	
+	must(cmd.Run())
 }
 
 func child() {
+	fmt.Printf("Running %v as process %d\n", os.Args[2:], os.Getpid())
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 
+	//syscall.Chroot()
+	
+	must(cmd.Run())
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
